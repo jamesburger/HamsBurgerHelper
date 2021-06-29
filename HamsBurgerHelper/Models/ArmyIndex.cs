58fbb64fd97a04e872f1cd87ec3309f358bdf402
James Burger <burgerj723@gmail.com>
2021-06-29 15:44:20 -0400

    Created ArmyIndex.cs

---
+++ HamsBurgerHelper/Models/ArmyIndex.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HamsBurgerHelper.Models
+{
+    public class ArmyIndex
+    {
+        public Guid Id { get; set; }
+        
+        public string Name { get; set; }
+    }
+}
